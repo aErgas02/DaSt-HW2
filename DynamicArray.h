81a6588a75556ad2fb99b58ad955f08a54d9c4b4
aErgas02 <a.ergas@campus.technion.ac.il>
2022-12-26 19:34:57 +0200

    Dynamic Array

--- DynamicArray.h
+++ DynamicArray.h
@@ -6,23 +6,52 @@
 #define WET2_DYNAMICARRAY_H
 
 template <class T>
-DynamicArray{
+class DynamicArray{
 private:
-    T* arr;
-    int size;
+    T* arr = nullptr;
+    enum {N = 11};
+    int size = N, cur = 0;
+
+    void increase();
+
 public:
-    DynamicArray(){};
-    DynamicArray(int i) : size(i){arr = new T()[size]};
-    ~DynamicArray(){};
+    DynamicArray() {arr = new T[N]();};
+    explicit DynamicArray(int i) : size(i) {
+        if(i <= 0) size = N;
+        arr = new T[size]();
+    };
+    ~DynamicArray(){delete[] arr;};
     void insert(T);
+    void print();
+};
+
+template <class T>
+void DynamicArray<T>::insert(T obj){
+    arr[cur] = obj;
+    cur++;
+    if(cur == size){
+        this->increase();
+    }
 }
 
 template <class T>
-DynamicArray::insert(){
-
+void DynamicArray<T>::increase(){
+    T* new_arr = new T[size*2]();
+    for(int i = 0; i < size; i++){
+        new_arr[i] = arr[i];
+    }
+    delete[] arr;
+    arr = new_arr;
+    size *= 2;
 }
 
-
+template <class T>
+void DynamicArray<T>::print(){
+    for(int i = 0; i < size; i++){
+        std::cout << arr[i]<<" ";
+    }
+    std::cout << std::endl;
+}
 
 
 #endif //WET2_DYNAMICARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "DynamicArray.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    DynamicArray<int> my_array(5);
+    my_array.insert(1);
+    my_array.insert(2);
+    my_array.insert(3);
+    my_array.insert(4);
+    my_array.print();
+    my_array.insert(5);
+    my_array.print();
+    my_array.insert(6);
+    my_array.insert(7);
+    my_array.insert(1);
+    my_array.insert(2);
+    my_array.print();
+    my_array.insert(3);
+    my_array.insert(4);
+    my_array.print();
     return 0;
 }
